820e76d26445276b3d5cf8d6e069e4789c54d8ec
Bryan Tejada <bryanenid97@gmail.com>
2020-08-06 12:56:11 -0400

    initial commit

--- main.js
+++ main.js
@@ -8,21 +8,28 @@ let PORT = 3031;
 
 http
   .createServer(async (req, res) => {
+    // Permission to access and fetch files from local machine
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-access-token");
     res.setHeader("Access-Control-Allow-Credentials", true);
 
+    // HTTP Queries ex. "localhost:3031/?scan=true"
     let { ui, document, image, scan } = url.parse(req.url, true).query;
 
+    // HTTP request is asking for a image
     if (req.headers["content-type"] == "image/jpeg") {
+      // Get a list of the file names in "/Output" folder
       const files_names = await fs.readdir(pathModule.normalize(`${process.cwd()}/Output/`));
+      // Filter files other than .jpf files
       const jpg_file_names = files_names.filter((item) => item.includes("jpg"));
 
       try {
+        // Send the first jpg file in "/Output" folder
         const jpg_file = await fs.readFile(pathModule.normalize(`${process.cwd()}/Output/${jpg_file_names[0]}`));
         res.setHeader("Content-Type", "image/jpeg");
         res.writeHead(200);
+        // It need to be binary so you can parse it as a blob object (https://developer.mozilla.org/en-US/docs/Web/API/Body/blob)
         res.end(jpg_file, "binary");
         return;
       } catch (err) {
@@ -31,11 +38,15 @@ http
         res.end();
         return;
       }
-    } else if (req.headers["content-type"] == "application/pdf" || document == "true") {
+    }
+    // HTTP request is asking for a pdf file
+    else if (req.headers["content-type"] == "application/pdf" || document == "true") {
       try {
-        res.setHeader("Content-Type", "application/pdf");
+        // Send the Output.pdf file from "/Output" folder
         let doc = await fs.readFile(pathModule.normalize(`${process.cwd()}/Output/output.pdf`));
+        res.setHeader("Content-Type", "application/pdf");
         res.writeHead(200);
+        // It need to be binary so you can parse it as a blob object (https://developer.mozilla.org/en-US/docs/Web/API/Body/blob)
         res.end(doc, "binary");
         return;
       } catch (err) {
@@ -46,15 +57,20 @@ http
       }
     }
 
+    // HTTP request is asking to open the Scanner Settings UI
     if (ui == "true") {
       console.log(`Scanner UI (Options)- ${new Date().toLocaleTimeString()} : ${new Date().toLocaleDateString()} `);
+      // Run batch script to open it minimized
       exec("cd commands && settings.bat", (err, stdout, stderr) => {
         res.writeHead(204);
         res.end();
         return;
       });
-    } else if (ui == "false" && scan == "true") {
+    }
+    // HTTP request is asking to run scanner
+    else if (ui == "false" && scan == "true") {
       console.log(`Running Scanner - ${new Date().toLocaleTimeString()} : ${new Date().toLocaleDateString()} `);
+      // Run batch script to open it minimized
       exec("cd commands && start.bat", (err, stdout, stderr) => {
         if (err) {
           console.error(err.message);
